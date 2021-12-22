d8c9bc72b34eb97ef803f1cd133ceab3badb710d
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-12-22 16:38:52 +0100

    Add serie on Mylist

--- databse.js
+++ databse.js
@@ -17,6 +17,34 @@ class Database {
         return shows
     }
 
+    async getShow(id) {
+        const client = await this.mongoclient.connect(this.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db(this.database).collection(this.collection)
+        const show = await db.find({
+            _id: id
+        })
+        client.close()
+        return show
+    }
+
+    async addShow(show) {
+        const client = await this.mongoclient.connect(this.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db(this.database).collection(this.collection)
+        const newShow = await db.insertOne(show)
+        client.close()
+        return newShow
+    }
+
+
+
+
+
 }
 
 export {
--- index.js
+++ index.js
@@ -21,3 +21,15 @@ app.get("/shows", async (req, res) => {
     let shows = await db.getAllShows()
     res.send(shows)
 })
+
+app.get("/shows/:id", async (req, res) => {
+    let id = new ObjectId(req.params.id)
+    let show = await db.getShow(id)
+    res.send(show)
+})
+
+app.post("/shows/add", async (req, res) => {
+    let show = req.body
+    let addedShow = await db.addShow(show)
+    res.send(addedShow)
+})
